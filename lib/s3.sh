33afe018f027460ba749ee3af7ac42b2b6166550
aespinosa <aespinosa@teachnova.com>
2013-10-05 12:55:27 +0200

    Now we will use S3cmd toolkit

--- lib/s3.sh
+++ lib/s3.sh
@@ -17,29 +17,54 @@
 # You should have received a copy of the GNU General Public License
 # along with Server-Backup.  If not, see <http://www.gnu.org/licenses/>.
 
-BAK_S3_GET_BIN="/usr/bin/php $BAK_LIB_PATH/s3/aws-object_get.php $BAK_S3_BUCKET"
-BAK_S3_PUT_BIN="/usr/bin/php $BAK_LIB_PATH/s3/aws-object_put.php $BAK_S3_BUCKET"
-BAK_S3_EXISTS_BIN="/usr/bin/php $BAK_LIB_PATH/s3/aws-object_exists.php $BAK_S3_BUCKET"
-BAK_S3_CONFIG_FILE="$BAK_LIB_PATH/s3/aws-php/config.inc.php"
-BAK_S3_CONFIG_DIST_FILE="$BAK_LIB_PATH/s3/aws-php/config-dist.inc.php"
+# BAK_S3_GET_BIN="/usr/bin/php $BAK_LIB_PATH/s3/aws-object_get.php $BAK_S3_BUCKET"
+# BAK_S3_PUT_BIN="/usr/bin/php $BAK_LIB_PATH/s3/aws-object_put.php $BAK_S3_BUCKET"
+# BAK_S3_EXISTS_BIN="/usr/bin/php $BAK_LIB_PATH/s3/aws-object_exists.php $BAK_S3_BUCKET"
+# BAK_S3_CONFIG_FILE="$BAK_LIB_PATH/s3/aws-php/config.inc.php"
+# BAK_S3_CONFIG_DIST_FILE="$BAK_LIB_PATH/s3/aws-php/config-dist.inc.php"
+
+BAK_S3_CMD_BIN='/usr/bin/s3cmd'
+BAK_S3_CONFIG_FILE="$BAK_CONFIG_PATH/.s3cfg"
+BAK_S3_GET_BIN="/usr/bin/s3cmd -c $BAK_S3_CONFIG_FILE get"
+BAK_S3_PUT_BIN="/usr/bin/s3cmd -c $BAK_S3_CONFIG_FILE put"
+BAK_S3_EXISTS_BIN="/usr/bin/s3cmd -c $BAK_S3_CONFIG_FILE info"
+BAK_S3_AUTOCHECK_BIN="/usr/bin/s3cmd -c $BAK_S3_CONFIG_FILE info"
+
+BAK_S3_BASE="s3://$BAK_S3_BUCKET/$BAK_S3_INSTANCE"
 
 BAK_S3_CURRENT_PATH=
 BAK_S3_ERROR=0
+
+s3_check() {
+   if [ ! $BAK_S3_ERROR -eq 0 ]; then return $BAK_S3_ERROR; fi 
+
+   if $BAK_S3_AUTOCHECK_BIN $BAK_S3_BASE 2>&1 | grep -q "ERROR"; then
+      return 1
+   fi
+   return $?
+}
 
 s3_config_show() {
    local error=0
    local status=
    local aws_status=
+   local s3cmd_status=
 
    if [ $BAK_S3_ERROR -eq 0 ]; then status="OK";
    else status="ERROR ($BAK_S3_ERROR)"; error=$BAK_S3_ERROR; fi
 
+   if [ -f "$BAK_S3_CMD_BIN" ]; then
+      s3cmd_status="OK"
+   else
+      s3cmd_status="ERROR : S3cmd is not installed"
+   fi
+
    if [ -f "$BAK_S3_CONFIG_FILE" ]; then
-      if grep -q "your-key-here" "$BAK_S3_CONFIG_FILE"; then
+      if s3_check; then
+         aws_status="OK"
+      else
          aws_status="ERROR : Invalid configuration, please set your credentials"
          error=1
-      else
-         aws_status="OK"
       fi
    else
       aws_status="ERROR : File not found"
@@ -49,7 +74,8 @@ s3_config_show() {
    cat << CONFIG
 S3 Configuration
 ------------------------------------------------
-Current file : $BAK_S3_CURRENT_FILE
+S3cmd        : $BAK_S3_CMD_BIN - $s3cmd_status
+Current file : $BAK_S3_CURRENT_FILE - $BAK_S3_CURRENT_PATH
 AWS config   : $BAK_S3_CONFIG_FILE - $aws_status
 Path         : [$BAK_S3_BUCKET]/$BAK_S3_INSTANCE
 Status       : $status
@@ -59,48 +85,104 @@ CONFIG
    return $error
 }
 
-s3_init() {
+s3_snapshot() {
+   local cfile="/tmp/$BAK_S3_CURRENT_FILE"
    local error=0
-   local date=
+   local date=`$DATE_BIN +%F`
 
-   if [ ! -f "$BAK_S3_CONFIG_FILE" ]; then
-      $CP_BIN "$BAK_S3_CONFIG_DIST_FILE" "$BAK_S3_CONFIG_FILE"
-   fi
-   $CHMOD_BIN 640 "$BAK_S3_CONFIG_FILE"
-   $CHOWN_BIN root:root "$BAK_S3_CONFIG_FILE"
+   if [ ! $BAK_S3_ERROR -eq 0 ]; then return $BAK_S3_ERROR; fi 
 
-   $BAK_S3_GET_BIN $BAK_S3_CURRENT_FILE "/tmp/$BAK_S3_CURRENT_FILE" > $BAK_NULL_OUTPUT 2>&1
+   $ECHO_BIN "$date" > "$cfile"
+   $ECHO_BIN "S3 : Setting new current file : '$date'" >> $BAK_OUTPUT
+   $BAK_S3_PUT_BIN "$cfile" "$BAK_S3_BASE/$BAK_S3_CURRENT_FILE" > $BAK_NULL_OUTPUT 2>&1
    error=$?
 
    if [ $error -eq 0 ]; then
-      # current file exists, use it
-      BAK_S3_CURRENT_PATH=`cat "/tmp/$BAK_S3_CURRENT_FILE"`
-   else
-      # current file does not exists, create it
-      date=`date +%F`
-      echo "$date" > "/tmp/$BAK_S3_CURRENT_FILE"
-      $BAK_S3_PUT_BIN $BAK_S3_CURRENT_FILE "/tmp/$BAK_S3_CURRENT_FILE" > $BAK_NULL_OUTPUT 2>&1
-      error=$?
+      BAK_S3_CURRENT_PATH="$date"
+   fi
 
-      if [ $error -eq 0 ]; then
-         BAK_S3_CURRENT_PATH="$date"
-      else
-         BAK_S3_ERROR=$error
+   $RM_BIN "$cfile"
+
+   return $error
+}
+
+s3_environment_check() {
+   if [ ! $BAK_S3_ERROR -eq 0 ]; then 
+      $ECHO_BIN "ERROR : Invalid S3 configuration, please set your credentials"
+      return $BAK_S3_ERROR; 
+   fi 
+   if [ ! -f "$BAK_S3_CMD_BIN" ]; then 
+      $ECHO_BIN "ERROR : S3cmd is not installed"
+      return 1; 
+   fi
+   return 0
+}
+
+s3_init() {
+   local error=0
+   local date=
+   local cfile="/tmp/$BAK_S3_CURRENT_FILE"
+
+   if [ -f "$BAK_S3_CONFIG_FILE" ]; then
+      $CHMOD_BIN 640 "$BAK_S3_CONFIG_FILE"
+      $CHOWN_BIN root:root "$BAK_S3_CONFIG_FILE"
+
+      s3_check
+      BAK_S3_ERROR=$?
+
+      if [ $BAK_S3_ERROR -eq 0 ]; then
+         $BAK_S3_GET_BIN "$BAK_S3_BASE/$BAK_S3_CURRENT_FILE" "$cfile" > $BAK_NULL_OUTPUT 2>&1
+         error=$?
+
+         if [ $error -eq 0 ]; then
+            # current file exists, use it
+            BAK_S3_CURRENT_PATH=`cat "$cfile"`
+         else
+            # current file does not exists, create it
+            date=`$DATE_BIN +%F`
+            $ECHO_BIN "$date" > "$cfile"
+            $BAK_S3_PUT_BIN "$cfile" "$BAK_S3_BASE/$BAK_S3_CURRENT_FILE" > $BAK_NULL_OUTPUT 2>&1
+            error=$?
+
+            if [ $error -eq 0 ]; then
+               BAK_S3_CURRENT_PATH="$date"
+            else
+               BAK_S3_ERROR=$error
+            fi
+         fi
+
+         $RM_BIN "$cfile"
       fi
+   else
+      BAK_S3_ERROR=1
    fi
+}
 
-   if [ -n "$BAK_S3_INSTANCE" ]; then
-      BAK_S3_CURRENT_PATH="$BAK_S3_CURRENT_PATH/$BAK_S3_INSTANCE"
+s3_get() {
+   local error=0
+   local file=$1
+   local name=`basename $file`
+
+   if [ ! $BAK_S3_ERROR -eq 0 ]; then return $BAK_S3_ERROR; fi 
+
+   if [ -f "$file" ]; then
+      $BAK_S3_GET_BIN "$BAK_S3_BASE/$BAK_S3_CURRENT_PATH/$name" "$file" > $BAK_NULL_OUTPUT 2>&1
+      error=$?
+   else
+      error=1
    fi
-   $RM_BIN "/tmp/$BAK_S3_CURRENT_FILE"
+   return $error
 }
 
 s3_put() {
-   error=0
-   file=$1
-   name=`basename $file`
+   local error=0
+   local file=$1
+   local name=`basename $file`
+
+   if [ ! $BAK_S3_ERROR -eq 0 ]; then return $BAK_S3_ERROR; fi 
+
    if [ -f "$file" ]; then
-      $BAK_S3_PUT_BIN "$BAK_S3_CURRENT_PATH/$name" "$file" > $BAK_NULL_OUTPUT 2>&1
+      $BAK_S3_PUT_BIN "$file" "$BAK_S3_BASE/$BAK_S3_CURRENT_PATH/$name"  > $BAK_NULL_OUTPUT 2>&1
       error=$?
    else
       error=1
