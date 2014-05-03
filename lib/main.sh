bbf89e8009c4b975ecd9d8d1f6b7a88e098c79bf
aespinosa <aespinosa@teachnova.com>
2014-05-03 10:36:14 +0200

    Bugfix

--- lib/main.sh
+++ lib/main.sh
@@ -189,7 +189,7 @@ old_files_rm () {
 
 mysql_check() {
    $ECHO_BIN -n "MySQL status: " >> $BAK_OUTPUT
-   $ECHO_BIN "-----------------------------------------------" >> $BAK_OUTPUT_EXTENDED
+   $ECHO_BIN "- MYSQL Status --------------------------------" >> $BAK_OUTPUT_EXTENDED
    service mysql status >> $BAK_OUTPUT_EXTENDED 2>&1
    error=$?
    $ECHO_BIN "-----------------------------------------------" >> $BAK_OUTPUT_EXTENDED
